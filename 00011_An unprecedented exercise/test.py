4d6087e0b6b93e19bd3694eda92420833cd59aae
franco@mumuki.org <franco@mumuki.org>
2023-03-22 18:43:28 +0000

    Mumuki Export on 2023-03-22 18:43:28 +0000

--- 00011_An unprecedented exercise/test.py
+++ 00011_An unprecedented exercise/test.py
@@ -1,9 +1,9 @@
   
   def test_can_focus_te_95_True_is_True(self):
-    self.assertTrue(can_focus("tea",95, True))
+    self.assertTrue(can_focus("tea", 95, True))
 
   def test_can_focus_tea_100_True_is_True(self):
-    self.assertTrue(can_focus("tea",100, True))
+    self.assertTrue(can_focus("tea", 100, True))
 
   def test_can_focus_mate_80_True_is_True(self):
     self.assertTrue(can_focus('mate', 80, True))
@@ -12,8 +12,8 @@
     self.assertFalse(can_focus('mate', 70, True))
 
   def test_can_focus_tea_94_True_is_False(self):
-    self.assertFalse(can_focus("tea",94, True))
+    self.assertFalse(can_focus("tea", 94, True))
 
   def test_can_focus_tea_95_False_is_False(self):
-    self.assertFalse(can_focus("tea",95, False))
+    self.assertFalse(can_focus("tea", 95, False))
     
