ec8359399249b40c09aba3fb5a562f6653a1fc8b
franco@mumuki.org <franco@mumuki.org>
2023-03-27 18:43:13 +0000

    Mumuki Export on 2023-03-27 18:43:13 +0000

--- 00005_Let's play TEG!/extra.py
+++ 00005_Let's play TEG!/extra.py
@@ -1,3 +1,2 @@
-def gano(cumplio_objetivo_secreto, cantidad_de_paises_conquistados):
-  return cumplio_objetivo_secreto or cantidad_de_paises_conquistados >= 30
-
+def won(met_secret_goal, number_of_countries_conquered):
+  return met_secret_objective or number_of_countries_conquered >= 30
