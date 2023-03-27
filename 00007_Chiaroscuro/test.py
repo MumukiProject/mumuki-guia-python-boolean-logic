4923f56e259569db082fb5d47539f517a66beb4f
franco@mumuki.org <franco@mumuki.org>
2023-03-27 20:16:49 +0000

    Mumuki Export on 2023-03-27 20:16:49 +0000

--- 00007_Chiaroscuro/test.py
+++ 00007_Chiaroscuro/test.py
@@ -1,12 +1,15 @@
-  
-  def test_tiene_contraste_blanco_negro_es_verdadero(self):
-    self.assertTrue(tiene_contraste("blanco","negro"))
+    
+  def test_has_contrast_white_black_is_true(self):
+    self.assertTrue(has_contrast("white","black"))
 
-  def test_tiene_contraste_beige_rosa_es_falso(self):
-    self.assertFalse(tiene_contraste("beige","rosa"))
+  def test_has_contrast_black_black_is_true(self):
+    self.assertTrue(has_contrast("black","black"))
 
-  def test_tiene_contraste_rojo_azul_es_falso(self):
-    self.assertFalse(tiene_contraste("rojo","azul"))
+  def test_has_contrast_beige_pink_is_false(self):
+    self.assertFalse(has_contrast("beige","pink"))
 
-  def test_tiene_contraste_negro_amarillo_es_verdadero(self):
-    self.assertTrue(tiene_contraste("negro","amarillo"))
+  def test_has_contrast_pink_blue_is_false(self):
+    self.assertFalse(has_contrast("pink","blue"))
+
+  def test_has_contrast_black_yellow_is_true(self):
+    self.assertTrue(has_contrast("black","yellow"))
