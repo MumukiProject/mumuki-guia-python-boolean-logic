00b7b0ef6e0932699bb82e5a2e514fab4350eb43
franco@mumuki.org <franco@mumuki.org>
2023-03-28 18:29:56 +0000

    Mumuki Export on 2023-03-28 18:29:56 +0000

--- 00012_Can I ride_/test.py
+++ 00012_Can I ride_/test.py
@@ -1,16 +1,16 @@
   
-  def test_una_persona_que_mide_1_5m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
-    self.assertTrue(puede_subirse(1.5, False, False))
+  def test_someone_who_is_1_5m_and_is_not_accompanied_by_an_adult_and_has_no_hearth_conditions_can_ride(self):
+    self.assertTrue(can_ride(1.5, False, False))
   
-  def test_una_persona_que_mide_1_7m_no_esta_acompaniada_por_un_adulto_y_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.7, False, True))
+  def test_someone_who_is_1_7m_and_is_not_accompanied_by_an_adult_y_has_a_hearth_condition_cannot_ride(self):
+    self.assertFalse(can_ride(1.7, False, True))
   
-  def test_una_persona_que_mide_1_2m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_puede_subirse_a_la_atraccion(self):
-    self.assertTrue(puede_subirse(1.2, True, False))
+  def test_someone_who_is_1_2m_and_is_accompanied_by_an_adult_and_has_no_hearth_conditions_can_ride(self):
+    self.assertTrue(can_ride(1.2, True, False))
   
-  def test_una_persona_que_mide_1_2m_no_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.2, False, False))
+  def test_someone_who_is_1_2m_and_is_not_accompanied_by_an_adult_and_has_no_hearth_conditions_cannot_ride(self):
+    self.assertFalse(can_ride(1.2, False, False))
   
-  def test_una_persona_que_mide_1_1m_esta_acompaniada_por_un_adulto_y_no_tiene_afecciones_cardiacas_no_puede_subirse_a_la_atraccion(self):
-    self.assertFalse(puede_subirse(1.1, True, False))
+  def test_someone_who_is_1_1m_and_is_accompanied_by_an_adult_and_has_no_hearth_conditions_cannot_ride(self):
+    self.assertFalse(can_ride(1.1, True, False))
   
